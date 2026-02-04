20da21027002a8b1e1d46278287175c138111b24
Frazer Harness <654994@eastridingcollege.ac.uk>
2026-02-04 23:17:26 +0000

    feedback page done

---
+++ feedback.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Feedback</title>
+  <link rel="stylesheet" href="main.css">
+  <link rel="stylesheet" href="feedback.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cambo&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+        <nav>
+        <ul>
+            <li>
+                <h1><a href="index.html">Managers Lounge</a></h1>
+            </li>
+            <li>
+                <h2><a href="index.html">Latest News</a></li>
+            </h2>
+            <li>
+                <h2><a href="index.html">Guides</a></li>
+            </h2>
+            <li>
+                <h2><a href="index.html">Discussion Board</a></li>
+            </h2>
+            <li>
+                <h2><a class="right-float" href="index.html">Login</a></li>
+            </h2>
+            <li>
+                <h2><a class="right-float" href="index.html">Sign Up</a></li>
+            </h2>
+        </ul>
+    </nav>
+<h1 class="title"><u>Feedback Form</u></h1>
+<div class="container">
+  <form action="feedback_validate.php" method="post">
+    <label class="labels" for="fname">First Name:</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label class="labels" for="lname">Last Name:</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label class="labels" for="username">Username:</label>
+    <input type="text" id="usrname" name="username" placeholder="Your username..">
+
+    <label class="labels" for="email">Email:</label>
+    <input type="text" id="email" name="email" placeholder="Your email..">
+
+    <label class="labels" for="subject">Subject:</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
+  
+</body>
+</html>
